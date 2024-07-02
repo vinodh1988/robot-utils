0cb2aae221facc150a5afe0e5ea2972e70bb63aa
vinodh1988 <k.c.vinodh@hotmail.com>
2024-07-02 20:26:30 +0530

    csvcheck

--- FileUtilities/moreFiles.py
+++ FileUtilities/moreFiles.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import csv
 def logDictionary(filepath,dict):
     try:
         with open(filepath, 'a') as file:
@@ -9,5 +10,16 @@ def logDictionary(filepath,dict):
         print(f"Error: {e}")
         raise e
     
+def read_csv_to_array_of_dictionary(file_path):
+    try:
+        array=[]
+        with open(file_path,mode="r",newline="",encoding="utf-8") as file:
+            reader=csv.DictReader(file)
+            for row in reader:
+                array.append(row)
+        return array
+    except Exception as e:
+        raise e
+
 
 
